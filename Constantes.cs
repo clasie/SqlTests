6268a0a9b6757cd945e376de5ea596b5d6a2e911
HP <HP@DESKTOP-R16LP3T>
2024-11-19 17:38:00 +0100

    Test

--- Constantes.cs
+++ Constantes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SqlTests
 {
-    public static class Constantes
+    public static class Constantes 
     {
         public const string CONN_STRING = @"Server=localhost\SQLEXPRESS;Database=BikeStore;Trusted_Connection=True;";
     }
